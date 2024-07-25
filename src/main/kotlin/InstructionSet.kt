478b602b033fdb79ea390a3ff3ba9cd10494a1d8
root <root@FinchComp>
2024-07-25 10:39:54 -0600

    built instructionSet

--- src/main/kotlin/InstructionSet.kt
+++ src/main/kotlin/InstructionSet.kt
@@ -1,4 +1,34 @@
 package org.example
 
+import org.example.instructions.*
+
 class InstructionSet {
+    private val instructions: MutableMap<String, Instruction> = mutableMapOf()
+
+    init {
+        // Register instructions
+        instructions["0"] = StoreInstruction()
+        instructions["1"] = AddInstruction()
+        instructions["2"] = SubInstruction()
+        instructions["3"] = ReadInstruction()
+        instructions["4"] = WriteInstruction()
+        instructions["5"] = JumpInstruction()
+        instructions["6"] = ReadKeyboardInstruction()
+        instructions["7"] = SwitchMemoryInstruction()
+        instructions["8"] = SkipEqualInstruction()
+        instructions["9"] = SkipEqualInstruction()
+        instructions["A"] = SetAInstruction()
+        instructions["B"] = SetTInstruction()
+        instructions["C"] = ReadTInstruction()
+        instructions["D"] = ConvertToBase10Instruction()
+        instructions["E"] = ConvertByteToAsciiInstruction()
+        instructions["F"] = DrawInstruction()
+
+    }
+
+    fun execute(instruction: String, cpu: CPU) {
+        val opcode = instruction[0].toString() // Get the opcode from the instruction
+        val instructionHandler = instructions[opcode]
+        instructionHandler?.execute(cpu, instruction) ?: throw IllegalArgumentException("Unknown instruction: $instruction")
+    }
 }
