3e4c18388427d6450826e165c8f7ecc1ed9b86e3
root <root@FinchComp>
2024-07-30 12:00:18 -0600

    SkipNotEqual written and tested

--- src/main/kotlin/instructions/SkipNotEqualInstruction.kt
+++ src/main/kotlin/instructions/SkipNotEqualInstruction.kt
@@ -4,6 +4,20 @@ import org.example.CPU
 
 class SkipNotEqualInstruction : Instruction() {
     override fun execute(cpu: CPU, instruction: String) {
+        // Extract register indices and value from the instruction
+        val xRegisterIndex = instruction[1].toString().toInt(16)
+        val yRegisterIndex = instruction[2].toString().toInt(16)
 
+
+        // Perform the comparison
+        val result = cpu.registers[xRegisterIndex] != cpu.registers[yRegisterIndex]
+
+        if (result) {
+            // If equal increment counter
+            cpu.programCounter.increment()
+        }
+
+        // Increment the program counter because all instructions do
+        cpu.programCounter.increment()
     }
 }
